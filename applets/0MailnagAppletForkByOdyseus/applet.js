3a7e5e33e08ca2fc1734dae80dd0a93e68e59acf
Odyseus <venusyadonis@gmail.com>
2018-06-13 01:02:10 -0300

    0MailnagAppletForkByOdyseus
    - Corrected settings changed callback function due to different amount of arguments passed by different Cinnamon versions. Not used by this applet, but doesn't hurt to have a function that actually works.

--- applets/0MailnagAppletForkByOdyseus/applet.js
+++ applets/0MailnagAppletForkByOdyseus/applet.js
@@ -662,8 +662,15 @@ MailnagAppletForkByOdyseusApplet.prototype = {
         }
     },
 
-    _onSettingsChanged: function(aPrefKey) { // jshint ignore:line
-        //
+    _onSettingsChanged: function(aPrefValue, aPrefKey) { // jshint ignore:line
+        // Note: On Cinnamon versions greater than 3.2.x, two arguments are passed to the
+        // settings callback instead of just one as in older versions. The first one is the
+        // setting value and the second one is the user data. To workaround this nonsense,
+        // check if the second argument is undefined to decide which
+        // argument to use as the pref key depending on the Cinnamon version.
+        // Mark for deletion on EOL. Cinnamon 3.2.x+
+        // Remove the following variable and directly use the second argument.
+        // let pref_key = aPrefKey || aPrefValue;
     }
 };
 
