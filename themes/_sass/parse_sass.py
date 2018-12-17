3665405a710b4f665655e53fa44e89ea88e50c3e
Odyseus <venusyadonis@gmail.com>
2018-12-17 15:37:18 -0300

    Themes
    - Metacity theme:
        - Fixed attached dialog titlebar. (Upstream fix)
    - Gtk3 theme:
        - Properly place the "grab" area for pane separators. (Upstream fix)
        - Style new overview classes. (Upstream fix)
        - Improve the titlebar styling. (Upstream fix)
        - Special case some headerbars. (Upstream fix)
        - Don't use pure white for file managers sidebar foregrounds. (Upstream fix)
        - Fixed some missing commas.
        - Eradication of single quotes.
        - Fixed some white space inconsistencies.
    - Cinnamon theme:
        - Added support for the grouped window list applet. (Upstream fix)
        - Use square menu favorites buttons. (Upstream fix)
        - Center align text in applets. (Upstream fix)
        - Updated the parse_sass.py script to generate on-the-fly from a template the SASS files used to build the Cinnamon theme CSS files.
        - Restructured SASS files to avoid breakages caused by mediocre code formatters.

--- themes/_sass/parse_sass.py
+++ themes/_sass/parse_sass.py
@@ -1,29 +1,47 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os
-from subprocess import call
 
-variant_names = ["GreybirdBlue", "MintGreen"]
-supported_cinnamon_versions = ["3.0", "3.4"]
+from subprocess import run
+
+variants = {
+    "GreybirdBlue": {
+        "color": "#398ee7"
+    },
+    "MintGreen": {
+        "color": "#accd8a"
+    }
+}
+supported_cinnamon_versions = ["3.0", "3.4", "4.0"]
 
 if __name__ == "__main__":
+    files_to_remove = []
     cmd = ["sass", "--no-source-map"]
 
-    for variant in variant_names:
-        # Parse Cinnamon themes
+    with open(os.path.abspath(os.path.join("cinnamon", "template.scss")), "r", encoding="UTF-8") as template_file:
+        template_data = template_file.read()
+
+    for variant_name, variant_data in variants.items():
         for cinnamon_version in supported_cinnamon_versions:
-            sass_file = os.path.abspath(os.path.join(
-                "cinnamon", "%s-%s.scss" % (variant, cinnamon_version)))
-            css_file = os.path.abspath(os.path.join("..", "_variants", variant, "_version_sensitive",
-                                                    "cinnamon", cinnamon_version, "cinnamon.css"))
+            sass_file_path = os.path.abspath(os.path.join(
+                "cinnamon", "%s-%s.scss" % (variant_name, cinnamon_version)))
+            sass_file_data = template_data.replace(
+                '"@cinnamon_version@"', cinnamon_version.replace(".", ""))
+            sass_file_data = sass_file_data.replace("@variant@", variant_name)
+            sass_file_data = sass_file_data.replace('"@selected_bg_color@"', variant_data["color"])
 
-            call(cmd + ["%s:%s" % (sass_file, css_file)])
-            # print(sass_file)
-            # print(css_file)
-            # if variant["name"] == "default":
-            #     call(cmd + ["%s:.cinnamon.css" % os.path.abspath(sass_file.name)])
-            # else:
-            #     variant_path = os.path.join(
-            #         all_variants_path, variant["name"], "cinnamon", "cinnamon.css")
+            with open(sass_file_path, "w", encoding="UTF-8") as sass_file:
+                sass_file.write(sass_file_data)
 
-            #     call(cmd + ["%s:%s" % (os.path.abspath(sass_file.name), variant_path)])
+            css_file_path = os.path.abspath(
+                os.path.join("..", "_variants", variant_name, "_version_sensitive",
+                             "cinnamon", cinnamon_version, "cinnamon.css"))
+
+            os.makedirs(os.path.dirname(css_file_path), exist_ok=True)
+
+            run(cmd + ["%s:%s" % (sass_file_path, css_file_path)])
+
+            files_to_remove.append(sass_file_path)
+
+    for file_path in files_to_remove:
+        os.remove(file_path)
