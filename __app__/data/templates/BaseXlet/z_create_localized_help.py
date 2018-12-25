f3cb8c8291cd5c4ca388785a92c6a808fc97aced
Odyseus <venusyadonis@gmail.com>
2018-12-25 00:00:22 -0300

    General
    - Updated sub-modules.
    - Updated manual page.
    - Organized import statements of the BaseXlet's z_create_localized_help.py script.

--- __app__/data/templates/BaseXlet/z_create_localized_help.py
+++ __app__/data/templates/BaseXlet/z_create_localized_help.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys
 import os
+import sys
 
 xlet_dir = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
 
@@ -14,8 +14,7 @@ app_folder = os.path.join(repo_folder, "__app__")
 if app_folder not in sys.path:
     sys.path.insert(0, app_folder)
 
-
-from python_modules.localized_help_creator import LocalizedHelpCreator, _, md
+from python_modules.localized_help_creator import LocalizedHelpCreator
 
 
 class Main(LocalizedHelpCreator):
