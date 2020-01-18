c03b4f47fc6e450d2b68db7ee7b7b86b9b97987c
Odyseus <venusyadonis@gmail.com>
2020-01-18 14:56:27 -0300

    Themes
    - General changes:
        - Made it easier for users to create their own theme variants. Read the documentation for instructions: Usage > app.py build_themes > How to create a custom theme variant?.
        - Removed MintGreen variant since I never used it and now users can create their own variant.
        - Exposed for configuration several contextual colors for customization when creating custom variants.
    - Gtk2 theme changes:
        - Removed unused image.
    - Gtk3 theme changes:
        - Xfce 4.14 fixes. (Upstream fix)
        - Add support for GtkStackSidebar needs attention. (Upstream fix)
        - Compliance with application's request to set a monospace font. (Upstream fix)
    - Cinnamon theme changes:
        - [GWL] Fixed justification of label inherited from applet-box. (Upstream fix)
        - [GWL] Adjusted grouped-window-list-thumbnail-menu becoming active. (Upstream fix)

--- themes/_variants/GreybirdBlue/config.py
+++ themes/_variants/GreybirdBlue/config.py
@@ -1,23 +1,47 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# These are convenience variables to store reusable data.
+selected_bg_color = "#398ee7"
+warning_color = "#fdde76"
+error_color = "#cc0000"
+link_color = "#0000ee"
+
+# For this file to be usable, the settings variable should be declared and
+# all data inside the settings dictionary is mandatory.
 settings = {
+    # The following 4 keys are used when parsing the Cinnamon theme SASS files.
+    "selected_bg_color": selected_bg_color,
+    "warning_color": warning_color,
+    "error_color": error_color,
+    "link_color": link_color,
+    # This replacement data is used at themes build time to perform string substitutions.
     "replacement_data": [
-        ("@selected_bg_color@", "#398ee7"),
-        ("@selected_bg_color_rgb@", "57, 142, 231"),
-        ("@info_color@", "#398ee7"),
-        ("@gradient_button_focus_1@", "#398ee7"),
-        ("@gradient_button_focus_2@", "#2f73b8"),
-        ("@gradient_iconview_selected_1@", "#398ee7"),
-        ("@gradient_iconview_selected_2@", "#357cc4"),
-        ("@gradient_progress_bar_1@", "#398ee7"),
+        # Selected background color. Used by all themes.
+        # This is basically the primary accent color.
+        ("@selected_bg_color@", selected_bg_color),
+        # Color that represents an HTML link. Used by all themes.
+        ("@link_color@", link_color),
+        # Color that represents information. Used by GTK3 theme.
+        ("@info_color@", selected_bg_color),
+        # Color that represents a question. Used by GTK3 theme.
+        ("@question_color@", "#55c1ec"),
+        # Color that represents a warning. Used by Cinnamon and GTK3 themes.
+        ("@warning_color@", warning_color),
+        # Color that represents an error. Used by Cinnamon and GTK3 themes.
+        ("@error_color@", error_color),
+        # Color that represents success. Used by GTK3 theme.
+        ("@success_color@", "#4e9a06"),
+        # Gradient colors for the image gtk-2.0/images/treeview/iconview-selected.svg
+        ("@gradient_iconview_selected_1@", selected_bg_color),
+        ("@gradient_iconview_selected_2@", "#3477bc"),
+        # Gradient colors for the image gtk-2.0/images/treeview/progress-bar.svg
+        ("@gradient_progress_bar_1@", selected_bg_color),
         ("@gradient_progress_bar_2@", "#3477bc"),
-        ("@gradient_row_selected_1@", "#398ee7"),
-        ("@gradient_row_selected_2@", "#3477bc"),  # 272925
-        ("@gradient_switch_on@", "#398ee7"),
-        ("@gradient_pressed_1@", "#398ee7"),
-        ("@gradient_pressed_2@", "#439cf7"),
-    ],
-    # Not used yet.
-    "options": {}
+        # Gradient colors for the image gtk-2.0/images/treeview/row-selected.svg
+        ("@gradient_row_selected_1@", selected_bg_color),
+        ("@gradient_row_selected_2@", "#3477bc"),
+        # Color for the image cinnamon/assets/switch-on.svg
+        ("@gradient_switch_on@", selected_bg_color)
+    ]
 }
---
+++ themes/_variants/README.md
@@ -0,0 +1,4 @@
+
+## How to create a custom theme variant
+
+Read the documentation for instructions: Usage > app.py build_themes > How to create a custom theme variant?
